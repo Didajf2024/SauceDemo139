a338985d476b3bb10d4cb534b7a7e89c34c55029
Didajf2024 <diegojf09@hotmail.com>
2025-06-25 20:30:24 -0300

    Concluido o mapeamento de elementos

--- Pages/InventoryPage.cs
+++ Pages/InventoryPage.cs
@@ -6,9 +6,15 @@ namespace Pages
     public class InventoryPage : CommonPage
     {
         // Mapeamento dos Elementos
-
+        // Como queremos mapear o botao "Add to Cart" e o id muda pro produto
+        // Iremos mapear de forma dinamica pelo bloco de ações
         // Construtor
-        public InventoryPage(IWebDriver driver) : base(driver){}
+        public InventoryPage(IWebDriver driver) : base(driver) { }
         // Ações
+        public void AdicionarAoCarrinho(String produto)
+        {
+            String seletorProduto = "add-to-cart-" + produto.ToLower().Replace("", "-");
+            driver.FindElement(By.Id(seletorProduto)).Click();
+        }
     }
 }
